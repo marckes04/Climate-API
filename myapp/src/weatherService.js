70178bc684ac5ad48171beff70db7820f7af68ff
marckes04 <maresp95@gmail.com>
2022-08-02 21:47:52 -0500

    first commit

---
+++ myapp/src/weatherService.js
@@ -0,0 +1,41 @@
+import { IconBase } from "react-icons/lib";
+
+const API_KEY = '988391fdd87f02de3fbaf0fb204819af'
+
+const makeIconURL = (IconId) => `https://openweathermap.org/img/wn/
+${IconId}@2x.png`
+
+const getFormattedWatherData = async(city, units = 'metric') =>{
+
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`
+
+    const data = await fetch(URL)
+    .then((res)=> res.json())
+    .then((data) => data);
+
+    const {weather, main: {temp, feels_like,temp_min,temp_max,
+    pressure,humidity},
+    wind: {speed},
+    sys: {country},
+    name,
+} = data;
+
+const {description,icon} = weather[0]
+
+return {
+description, 
+iconURL: makeIconURL(icon),
+temp,
+feels_like, 
+temp_min, 
+temp_max, 
+pressure,
+humidity,
+speed,
+country,
+name,
+};
+
+};
+
+export {getFormattedWatherData};
